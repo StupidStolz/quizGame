39b238826f342392daa7bf1284cbf93a32ba128b
stupidstolz <stupid.freelancer42@gmail.com>
2021-05-30 01:12:31 +0300

    quiz app

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'choose right number',
+        a: '12',
+        b: '28',
+        c: '97',
+        d: '42',
+        correct: 'd'
+    }, {
+        question: 'how much do you need sleep to feel good',
+        a: '1-2hr',
+        b: '20-40min',
+        c: '6-7hr',
+        d: '13-15',
+        correct: 'c'
+    }, {
+        question: 'who president of USA right now',
+        a: 'Donald Trump',
+        b: 'Joe Biden',
+        c: 'George Bush',
+        d: 'Gachi Muchi',
+        correct: 'd'
+    }, {
+        question: 'how much time i need to join akvelon?)',
+        a: 'few days',
+        b: '2-3 month',
+        c: '10 year',
+        d: 'alredy invite from all from FANG...',
+        correct: 'b'
+    }, {
+        question: 'Never',
+        a: 'say never',
+        b: 'gonna give you up',
+        c: 'alone',
+        d: 'land',
+        correct: 'b'
+    }
+]
